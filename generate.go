25c6253390a7a0f296821af0fb1d24623d8dd7dc
Danny Hermes <dhermes@blend.com>
2021-08-12 22:39:37 -0500

    Merging `generate.go` + `suite.go`.
    Primary goal is to gather the differences from the
    original `golembic`.

--- generate.go
+++ generate.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/blend/go-sdk/db"
 	"github.com/blend/go-sdk/db/migration"
+	"github.com/blend/go-sdk/ex"
 )
 
 // NOTE: Ensure that
@@ -81,4 +82,29 @@ type applyAction struct {
 // Action executes ApplyMigration for a given migration.
 func (aa *applyAction) Action(ctx context.Context, pool *db.Connection, tx *sql.Tx) error {
 	return aa.m.ApplyMigration(ctx, pool, tx, aa.Migration)
+}
+
+// ApplyDynamic applies a migrations suite. Rather than using a `range`
+// over `s.Groups`, it uses a length check, which allows `s.Groups` to
+// change dynamically during the iteration.
+func ApplyDynamic(ctx context.Context, s *migration.Suite, c *db.Connection) (err error) {
+	defer s.WriteStats(ctx)
+	defer func() {
+		if r := recover(); r != nil {
+			err = ex.New(r)
+		}
+	}()
+
+	for i := 0; i < len(s.Groups); i++ {
+		group := s.Groups[i]
+		if err = group.Action(migration.WithSuite(ctx, s), c); err != nil {
+			return
+		}
+	}
+	return
+}
+
+// alwaysPredicate can be used to always run an action.
+func alwaysPredicate(_ context.Context, _ *db.Connection, _ *sql.Tx) (bool, error) {
+	return true, nil
 }
