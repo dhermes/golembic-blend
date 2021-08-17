ee19ace7fed3c89e5a637d948904e13887c7c6f2
Danny Hermes <dhermes@blend.com>
2021-08-17 09:23:24 -0500

    Add test for invalid migration in table.

--- generate_test.go
+++ generate_test.go
@@ -55,7 +55,7 @@ func TestGenerateSuite_HappyPath(t *testing.T) {
 	logLines := []string{
 		fmt.Sprintf("[db.migration] -- applied -- Check table does not exist: %s", mt),
 		"[db.migration] -- plan -- Determine migrations that need to be applied",
-		"[db.migration] -- aa60f058f5f5 -- Create table",
+		"[db.migration] -- aa60f058f5f5 -- Create first table",
 		"[db.migration] -- ab1208989a3f -- Alter first table",
 		"[db.migration] -- 60a33b9d4c77 -- Add second table",
 		"[db.migration.stats] 4 applied 0 skipped 0 failed 4 total",
@@ -101,11 +101,70 @@ func TestGenerateSuite_HappyPath(t *testing.T) {
 	logBuffer.Reset()
 }
 
+func TestGenerateSuite_StaleCheckout(t *testing.T) {
+	it := assert.New(t)
+
+	ctx := context.TODO()
+	pool := defaultDB()
+	it.NotNil(pool)
+
+	// Happy path once (no need to verify logs, see above)
+	suffix := anyLowercase(6)
+	mt := fmt.Sprintf("foo_%s_migrations", suffix)
+	t1 := fmt.Sprintf("foo1_%s", suffix)
+	t2 := fmt.Sprintf("foo2_%s", suffix)
+	t.Cleanup(func() {
+		err1 := dropTable(ctx, pool, mt)
+		err2 := dropTable(ctx, pool, t1)
+		err3 := dropTable(ctx, pool, t2)
+		it.Nil(err1)
+		it.Nil(err2)
+		it.Nil(err3)
+	})
+	var logBuffer bytes.Buffer
+	log := logger.Memory(&logBuffer)
+
+	migrations, err := makeSequence(t1, t2)
+	it.Nil(err)
+	m, err := golembic.NewManager(
+		golembic.OptManagerSequence(migrations),
+		golembic.OptManagerMetadataTable(mt),
+		golembic.OptManagerLog(log),
+	)
+	it.Nil(err)
+	suite, err := golembic.GenerateSuite(m)
+	it.Nil(err)
+	err = golembic.ApplyDynamic(ctx, suite, pool)
+	it.Nil(err)
+	logBuffer.Reset()
+
+	// Introduce "new" migration that has not yet been seen
+	statement := fmt.Sprintf(
+		"INSERT INTO %s (serial_id, revision, previous) VALUES ($1, $2, $3)",
+		golembic.QuoteIdentifier(mt),
+	)
+	_, err = pool.Invoke(db.OptContext(ctx)).Exec(statement, 3, "not-in-sequence", "60a33b9d4c77")
+	it.Nil(err)
+
+	suite, err = golembic.GenerateSuite(m)
+	it.Nil(err)
+	err = golembic.ApplyDynamic(ctx, suite, pool)
+	it.Equal(`No migration registered for revision; Revision: "not-in-sequence"`, fmt.Sprintf("%v", err))
+	logLines := []string{
+		fmt.Sprintf("[db.migration] -- skipped -- Check table does not exist: %s", mt),
+		"[db.migration] -- plan -- Determine migrations that need to be applied",
+		"[db.migration.stats] 0 applied 1 skipped 0 failed 1 total",
+		"",
+	}
+	it.Equal(strings.Join(logLines, "\n"), logBuffer.String())
+	logBuffer.Reset()
+}
+
 func makeSequence(t1, t2 string) (*golembic.Migrations, error) {
 	ct1 := fmt.Sprintf("CREATE TABLE %s ( bar TEXT )", golembic.QuoteIdentifier(t1))
 	root, err := golembic.NewMigration(
 		golembic.OptRevision("aa60f058f5f5"),
-		golembic.OptDescription("Create table"),
+		golembic.OptDescription("Create first table"),
 		golembic.OptUpFromSQL(ct1),
 	)
 	if err != nil {
